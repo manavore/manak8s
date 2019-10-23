c3aae33f3c230e5033d4ce90ccd3c502bc4e03bb
Etienne Napoleone <etienne.napoleone@gmail.com>
2019-10-23 15:19:23 +0900

    feat: retire cluster

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,3 @@
 provider "digitalocean" {
   token = "${var.do_token}"
 }
-
-provider "kubernetes" {
-  host = "${digitalocean_kubernetes_cluster.cluster.endpoint}"
-
-  client_certificate     = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_certificate)}"
-  client_key             = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_key)}"
-  cluster_ca_certificate = "${base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)}"
-}
-
-resource "digitalocean_kubernetes_cluster" "cluster" {
-  name    = "manak8s"
-  region  = "fra1" # Frankfurt 1
-  version = "1.15.3-do.2"
-
-  node_pool {
-    name       = "pool1"
-    size       = "s-1vcpu-2gb"
-    node_count = 1
-  }
-}
