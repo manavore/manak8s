9857238dee704f6caaa8f0d6edf56df224d1a7db
etienne-napoleone <etienne.napoleone@gmail.com>
2020-01-12 23:02:09 +0900

    fix(provider): use correct ressource name

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -4,10 +4,10 @@ provider "digitalocean" {
 }
 
 provider "kubernetes" {
-  host    = digitalocean_kubernetes_cluster.cluster.endpoint
+  host    = digitalocean_kubernetes_cluster.this.endpoint
   version = "~> 1.10"
 
-  client_certificate     = base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.cluster.kube_config.0.cluster_ca_certificate)
+  client_certificate     = base64decode(digitalocean_kubernetes_cluster.this.kube_config.0.client_certificate)
+  client_key             = base64decode(digitalocean_kubernetes_cluster.this.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(digitalocean_kubernetes_cluster.this.kube_config.0.cluster_ca_certificate)
 }
