49dfd672351d89b79dceefab12a11af3e18d85be
etienne-napoleone <etienne.napoleone@gmail.com>
2020-01-12 23:59:50 +0900

    feat(proxy): add lb and traefik

--- terraform/proxy.tf
+++ terraform/proxy.tf
@@ -27,6 +27,29 @@ resource "kubernetes_deployment" "proxy" {
           image = "traefik:1.2"
           name  = "proxy"
 
+          port {
+            name           = "http"
+            container_port = 80
+            protocol       = "TCP"
+          }
+
+          port {
+            name           = "https"
+            container_port = 443
+            protocol       = "TCP"
+          }
+
+          port {
+            name           = "dashboard"
+            container_port = 8080
+            protocol       = "TCP"
+          }
+
+          volume_mount {
+            mount_path = "/etc/traefik/"
+            name       = "proxy-config"
+          }
+
           liveness_probe {
             http_get {
               path = "/ping"
@@ -37,7 +60,24 @@ resource "kubernetes_deployment" "proxy" {
             period_seconds        = 10
           }
         }
+        volume {
+          name = "proxy-config"
+          config_map {
+            name = "proxy-config"
+          }
+        }
       }
     }
+  }
+}
+
+resource "kubernetes_config_map" "proxy_config" {
+  metadata {
+    name = "proxy-config"
+  }
+
+  data = {
+    "traefik.toml"       = "${file("${path.module}/configs/traefik.toml")}"
+    "file_provider.toml" = "${file("${path.module}/configs/file_provider.toml")}"
   }
 }
